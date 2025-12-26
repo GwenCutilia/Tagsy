19b484723e52c0877c581122e5f80f309e72b59a
Gwencutilia <Gwencutilia@outlook.com>
2025-12-26 18:20:40 +0800

    2025.12.26

--- Module/Tagsy_Core.user.js
+++ Module/Tagsy_Core.user.js
@@ -7,8 +7,6 @@
 // @icon			https://www.emojiall.com/images/60/microsoft/2728.png
 // @match			http://127.0.0.1:5500/*
 // @require			http://127.0.0.1:5500/Module/Tagsy_Import.js
-// @match			file:///D:/Creat/VSCode/Tagsy_V2.0/Tagsy/*
-// @require			file:///D:/Creat/VSCode/Tagsy_V2.0/Tagsy/Module/Tagsy_Import.js
 // @match			https://weavefate.asia/*
 // @require			https://weavefate.asia/Module/Tagsy_Import.js
 // @match			https://qlabel.tencent.com/*
